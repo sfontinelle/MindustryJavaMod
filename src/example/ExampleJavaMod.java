e3b8e6c4132cc0163737f951e289383e660c06d3
Steven Fontinelle <1820251+sfontinelle@users.noreply.github.comwq>
2023-07-18 17:52:31 -0500

    change ui icons to ones created by albert

--- src/example/ExampleJavaMod.java
+++ src/example/ExampleJavaMod.java
@@ -1,36 +1,79 @@
 package example;
 
 import arc.*;
+import arc.scene.style.Drawable;
+import arc.scene.style.TextureRegionDrawable;
+import arc.struct.ObjectMap;
 import arc.util.*;
 import mindustry.*;
 import mindustry.content.*;
+import mindustry.core.UI;
 import mindustry.game.EventType.*;
 import mindustry.gen.*;
 import mindustry.mod.*;
+import mindustry.type.Category;
 import mindustry.ui.dialogs.*;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.description.method.ParameterDescription;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.implementation.bind.annotation.*;
 
-public class ExampleJavaMod extends Mod{
+import java.lang.reflect.Method;
+import java.util.concurrent.Callable;
 
-    public ExampleJavaMod(){
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
+public class ExampleJavaMod extends Mod {
+
+    boolean isLoaded = false;
+    boolean containsKey = false;
+
+    public ExampleJavaMod() {
         Log.info("Loaded ExampleJavaMod constructor.");
 
         //listen for game load event
         Events.on(ClientLoadEvent.class, e -> {
             //show dialog upon startup
             Time.runTask(10f, () -> {
-                BaseDialog dialog = new BaseDialog("frog");
-                dialog.cont.add("behold").row();
-                //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
-                dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
-                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
-                dialog.show();
+                loadIcons();
             });
         });
     }
 
+    public void startScreen() {
+        BaseDialog dialog = new BaseDialog("frog");
+        dialog.cont.add("behold").row();
+        dialog.cont.add("turret").row();
+        dialog.cont.add("" + Icon.icons.size).row();
+        dialog.cont.add("" + Icon.icons.size).row();
+        dialog.cont.add("isLoaded: " + isLoaded).row();
+        dialog.cont.add("containsKey?: " + Icon.icons.containsKey("liquid")).row();
+        dialog.cont.add("containsKey?: " + containsKey).row();
+        //mod sprites are prefixed with the mod name (this mod is called 'example-java-mod' in its config)
+        dialog.cont.image(Core.atlas.find("example-java-mod-frog")).pad(20f).row();
+        dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+        dialog.show();
+    }
+
     @Override
-    public void loadContent(){
+    public void loadContent() {
         Log.info("Loading some example content.");
+    }
+
+    public void loadIcons() {
+        ObjectMap<String, TextureRegionDrawable> icons = Icon.icons;
+        icons.put("turret", getCustomIcons("turrets"));
+        icons.put("liquid", getCustomIcons("liquid"));
+        icons.put("defense", getCustomIcons("defense"));
+        icons.put("units", getCustomIcons("units"));
+        icons.put("production", getCustomIcons("production"));
+        icons.put("effect", getCustomIcons("utilities"));
+        icons.put("logic", getCustomIcons("logic"));
+        isLoaded = true;
+    }
+
+    public <T extends Drawable> T getCustomIcons(String name) {
+        return Core.atlas.getDrawable("example-java-mod-" + name);
     }
 
 }
